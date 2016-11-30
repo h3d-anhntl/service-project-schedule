eadad6b6dadd00c964c2f1a3efc6d39d1051f65d
lananhnt0912 <lananh.nt09@gmail.com>
2016-11-30 17:25:59 +0700

    ket noi voi flex bang frame fproject
    use fproject\rest\ActiveController

--- basic/controllers/CalendarController.php
+++ basic/controllers/CalendarController.php
@@ -2,11 +2,29 @@
 	namespace app\controllers;
  
 	
-	use yii\rest\ActiveController;
+	use app\models\Calendar; 	
+	use fproject\rest\ActiveController;
+	use yii\db\ActiveQuery;
 	
 	class CalendarController extends ActiveController
 	{
 		public $modelClass = 'app\models\Calendar';
+
+		/**
+	     * Condition to find all resources with relations.
+	     * Use '@findAllCondition' as the key for client-side condition
+	     * @param array $params
+	     * @return ActiveQuery
+	     */
+	    public function findAllCondition($params)
+	    {
+	        /** @var ActiveQuery $query */
+	        $query = \Yii::createObject(ActiveQuery::className(), [$this->modelClass]);
+
+	        return $query;
+	    }
 		
 	}
+
+
 ?>
--- basic/controllers/EmployeeController.php
+++ basic/controllers/EmployeeController.php
@@ -2,11 +2,27 @@
 	namespace app\controllers;
  
 	
-	use yii\rest\ActiveController;
-	
+	use app\models\Employee; 	
+	use fproject\rest\ActiveController;
+	use yii\db\ActiveQuery;
+
 	class EmployeeController extends ActiveController
 	{
 		public $modelClass = 'app\models\Employee';
+		
+		/**
+	     * Condition to find all resources with relations.
+	     * Use '@findAllCondition' as the key for client-side condition
+	     * @param array $params
+	     * @return ActiveQuery
+	     */
+	    public function findAllCondition($params)
+	    {
+	        /** @var ActiveQuery $query */
+	        $query = \Yii::createObject(ActiveQuery::className(), [$this->modelClass]);
+
+	        return $query;
+	    }
 		
 	}
 ?>
